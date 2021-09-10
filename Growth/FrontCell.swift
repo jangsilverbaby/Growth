8e48e23656f914809efdf47cb0c66d70bae8b214
eunaejang <dmsdo1234@gmail.com>
2021-09-10 20:52:11 +0900

    그림자 효과 삭제

--- Growth/FrontCell.swift
+++ Growth/FrontCell.swift
@@ -24,21 +24,5 @@ class FrontCell: UICollectionViewCell {
             // from being clipped to the corner radius
             layer.cornerRadius = cornerRadius
             layer.masksToBounds = false
-            
-            // Apply a shadow
-            layer.shadowRadius = 8.0
-            layer.shadowOpacity = 0.10
-            layer.shadowColor = UIColor.black.cgColor
-            layer.shadowOffset = CGSize(width: 0, height: 5)
-        }
-        
-        override func layoutSubviews() {
-            super.layoutSubviews()
-            
-            // Improve scrolling performance with an explicit shadowPath
-            layer.shadowPath = UIBezierPath(
-                roundedRect: bounds,
-                cornerRadius: cornerRadius
-            ).cgPath
-        }
+    }
 }
