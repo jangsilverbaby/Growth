d15afc3814b92c6b8644c70eb5c531a9eadf0c94
eunaejang <dmsdo1234@gmail.com>
2021-09-30 13:31:54 +0900

    공통으로 쓸 수 있는 프론트 데이터 리스트 생성

--- Growth/FrontListVC.swift
+++ Growth/FrontListVC.swift
@@ -10,19 +10,10 @@ import UIKit
 // UICollectionViewDataSource : 컬렉션 뷰의 셀은 총 몇 개?
 // UICollectionViewDelegate : 컬렉션 뷰를 어떻게 보여줄 것인가?
 class FrontVC: UIViewController {
+    // 앱 델리게이트 객체의 참조 정보를 읽어온다.
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
     let sectionInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-    let frontList: [FrontData] = [
-        FrontData(frontImg: UIImage(named: "pet.JPG")!, name: "먼지"),
-        FrontData(frontImg: UIImage(named: "plants.HEIC")!, name: "오이"),
-        FrontData(frontImg: UIImage(named: "food.HEIC")!, name: "오늘의 음식"),
-        FrontData(frontImg: UIImage(named: "sky.HEIC")!, name: "오늘의 하늘"),
-        FrontData(frontImg: UIImage(named: "study.PNG")!, name: "공부 기록"),
-        FrontData(frontImg: UIImage(named: "question.JPG")!, name: "하루에 한 문제"),
-        FrontData(frontImg: UIImage(named: "phrase.JPG")!, name: "하루에 한 글귀"),
-        FrontData(frontImg: UIImage(named: "ootd.JPG")!, name: "OOTD"),
-        FrontData(frontImg: UIImage(named: "diary.HEIC")!, name: "나의 일기")
-    ]
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -37,12 +28,12 @@ class FrontVC: UIViewController {
 extension FrontVC: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     // 컬렉션 뷰에 총 몇개의 벳울 표시할 것인가
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return frontList.count
+        return appDelegate.frontlist.count
     }
     
     // 해당 cell에 무슨 셀을 표시할 지 결정
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let item = frontList[indexPath.item]
+        let item = appDelegate.frontlist[indexPath.item]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "frontCell", for: indexPath) as! FrontCell
         
         cell.frontImgView.image = item.frontImg
