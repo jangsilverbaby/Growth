f97d0cc6cf02217d25313ef771af5a817f4e2c04
eunaejang <dmsdo1234@gmail.com>
2021-11-13 23:02:09 +0900

    table self sizing

--- Growth/ContentListVC.swift
+++ Growth/ContentListVC.swift
@@ -20,6 +20,8 @@ class ContentListVC: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 400
         self.navigationItem.title = record.value(forKey: "name") as? String
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -49,7 +51,8 @@ class ContentListVC: UITableViewController {
         dateFormatter.dateFormat = "yyyy.MM.dd E"
         cell.regdate.text = dateFormatter.string(from: object.value(forKey: "regdate") as! Date)
         cell.contents.text = object.value(forKey: "contents") as? String
-        cell.contentImage?.image = imageManager.getSavedImage(named: object.value(forKey: "image") as! String)
+        cell.contentImage?.image = imageManager.getSavedImage(named: object.value(forKey: "image") as! String)?.aspectFitImage(inRect: cell.contentImage.frame)
+        cell.contentImage?.contentMode = .top
         
         return cell
     }
@@ -62,6 +65,28 @@ class ContentListVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return CGFloat(110)
-    }    
+        return UITableView.automaticDimension
+    }
+
+}
+
+// 게시물 이미지를 크키에 맞게 resizing
+// 출처 : https://woongsios.tistory.com/106
+extension UIImage {
+    func aspectFitImage(inRect rect: CGRect) -> UIImage? {
+        let width = self.size.width
+        let height = self.size.height
+        let aspectWidth = rect.width / width
+        let aspectHeight = rect.height / height
+        let scaleFactor = aspectWidth > aspectHeight ? rect.size.height / height : rect.size.width / width
+
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width * scaleFactor, height: height * scaleFactor), false, 0.0)
+        self.draw(in: CGRect(x: 0.0, y: 0.0, width: width * scaleFactor, height: height * scaleFactor))
+
+        defer {
+            UIGraphicsEndImageContext()
+        }
+
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
 }
